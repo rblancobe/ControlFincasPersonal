d099b54d93bb22f94f53b3d33cfd6989e8bf5299
Jose Jibaja <jgibaja@hortichuelas.es>
2023-07-12 15:05:06 +0200

    20230712_1504

--- app/src/main/java/com/example/ControlFincasPersonal/Login.java
+++ app/src/main/java/com/example/ControlFincasPersonal/Login.java
@@ -1,5 +1,6 @@
 package com.example.ControlFincasPersonal;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,12 +11,24 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.channels.AsynchronousChannel;
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class Login extends AppCompatActivity {
 
     private EditText editTextUsername, editTextPassword;
-
+    private Context context = Login.this;
+    private AsyncHttpClient client;
     // Local
     private final String localUsername = "777";
     private final String localPassword = "777";
@@ -39,10 +52,10 @@ public class Login extends AppCompatActivity {
                 String password = editTextPassword.getText().toString();
 
                 // Mensajes de depuración
-                Log.d("Login", "Valor de username: " + 777);
-                Log.d("Login", "Valor de password: " + 777);
-                Log.d("Login", "Valor de localUsername: " + localUsername);
-                Log.d("Login", "Valor de localPassword: " + localPassword);
+                //Log.d("Login", "Valor de username: " + 777);
+                //Log.d("Login", "Valor de password: " + 777);
+                //Log.d("Login", "Valor de localUsername: " + localUsername);
+                //Log.d("Login", "Valor de localPassword: " + localPassword);
 
                 // Verificar si los campos están vacíos
                 if (username.isEmpty() || password.isEmpty()) {
@@ -51,16 +64,55 @@ public class Login extends AppCompatActivity {
                     // Los datos de inicio de sesión son correctos
 
                     // Redireccionar al activity_main.xml
-                    Intent intent = new Intent(Login.this, MainActivity.class);
-                    startActivity(intent);
+                    RequestParams params = new RequestParams();
+                    params.put("imei","");
+                    params.put("usr",username);
+                    params.put("pass",password);
+                    client = new AsyncHttpClient();
+
+                    client.post(utils.service(), params, new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                            try {
+                                JSONObject response = new JSONObject(new String(responseBody).trim());
+                                Toast.makeText(context,new String(responseBody),Toast.LENGTH_SHORT).show();
+                                /*String id = response.getString("log");
+                                JSONArray json = new JSONArray(id);
+                                JSONObject obj = json.getJSONObject(0);
+                                String valido = obj.getString("valido");
+
+                                if(valido.equals("UN")){
+                                    Toast.makeText(context,"El dispositivo utilizado no está autorizado\r\nContacte con el administrador",Toast.LENGTH_SHORT).show();
+                                }else if(valido.equals("NO")){
+                                    Toast.makeText(context,"Error en Usuario o contraseña",Toast.LENGTH_SHORT).show();
+                                }else{
+                                    goToMain();
+                                }*/
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                Toast.makeText(context,"Error JSON OBJ",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                            Toast.makeText(context,"ERROR RESPONSE",Toast.LENGTH_SHORT).show();
+                        }
+                    });
 
                     // FIN
-                    finish();
+                    //finish();
                 } else {
                     // Inicio de sesión incorrecto en usr o pass
                     Toast.makeText(Login.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    private  void goToMain(){
+        Toast.makeText(context,"Entro en GoToMain",Toast.LENGTH_SHORT).show();
+     //   Intent intent = new Intent(Login.this, MainActivity.class);
+     //   startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/ControlFincasPersonal/utils.java
@@ -0,0 +1,7 @@
+package com.example.ControlFincasPersonal;
+
+public  class utils {
+    public static String service(){
+        return "http://192.168.2.75/HortichuelasNet/CpFincas/service.php";
+    }
+}
